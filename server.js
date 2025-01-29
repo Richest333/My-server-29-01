dbc971cf1edc1568895ff0c5fc9d1cf1e6414c8f
Richest333 <fomychov.ivan2006@gmail.com>
2025-01-29 12:49:28 +0200

    Обновление файлов для развертывания

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const https = require('https');
 const fs = require('fs');
 const express = require('express');
-const cors = require('cors'); // Подключаем CORS
+const cors = require('cors');
 
 const app = express();
 
 // Разрешаем запросы со всех доменов
-app.use(cors()); 
+app.use(cors());
 
-// Проверяем наличие сертификатов (если они загружаются из файлов)
-const httpsOptions = process.env.NODE_ENV === 'production' ? {
-    key: fs.readFileSync('localhost-key.pem'),  // Путь к ключу
-    cert: fs.readFileSync('localhost.pem')      // Путь к сертификату
-} : {};  // В production мы будем использовать HTTPS, а в другом случае - только HTTP
+// Путь к сертификатам
+const httpsOptions = {
+    key: fs.readFileSync('localhost-key.pem'),
+    cert: fs.readFileSync('localhost.pem')
+};
 
 // Пример маршрута
 app.get('/', (req, res) => {
@@ -21,14 +21,8 @@ app.get('/', (req, res) => {
 
 // Запуск сервера
 const HOST = '0.0.0.0'; // Слушаем все IP
-const PORT = process.env.PORT || 443; // Используем PORT, предоставленный Render или 443 по умолчанию
+const PORT = process.env.PORT || 443; // Используем PORT, предоставленный платформой или 443 по умолчанию
 
-if (httpsOptions.key && httpsOptions.cert) {
-    https.createServer(httpsOptions, app).listen(PORT, HOST, () => {
-        console.log(`Сервер запущен: https://localhost:${PORT}`);
-    });
-} else {
-    app.listen(PORT, HOST, () => {
-        console.log(`Сервер работает без HTTPS на http://localhost:${PORT}`);
-    });
-}
+https.createServer(httpsOptions, app).listen(PORT, HOST, () => {
+    console.log(`Сервер запущен: https://localhost:${PORT}`);
+});
